fa8daffcd66037fc7a18562a9e5ed9cc601cc724
Domin737 <p.dominiak.pd@gmail.com>
2023-01-13 02:41:19 +0100

    .

--- script.js
+++ script.js
@@ -57,12 +57,12 @@ $(document).ready(function () {
     backSpeed: 60,
     loop: true,
   });
-  var typed = new Typed(".typing-banner", {
-    strings: ["Hello Wrold!"],
-    typeSpeed: 60,
-    backSpeed: 60,
-    loop: true,
-  });
+  //   var typed = new Typed(".typing-banner", {
+  //     strings: ["Hello Wrold! My name is"],
+  //     typeSpeed: 30,
+  //     backSpeed: 30,
+  //     loop: true,
+  //   });
 
   // owl carousel script
   $(".carousel").owlCarousel({
